8d5acb73f44cd07a20dc4503da6bcf848e51fedb
tatyana rutkowska <kotita49@hotmail.com>
2020-12-14 21:44:30 +0000

    Calculator methods and while loop

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -7,41 +7,100 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            PrintWelcomeMessage();
+            while (true)
+            {
+                calculateResult();
+            }
+        }
+
+        private static void PrintWelcomeMessage()
+        {
             Console.WriteLine("Welcome to the calculator!");
+        }
+
+        private static string enterOp()
+        {
             Console.Write("Please enter an operator");
             string op = Console.ReadLine();
-            Console.WriteLine("How many numbers do you want to " + op + " ?");
-            int amount = int.Parse(Console.ReadLine());
-            int[] numbers = new int[amount];
-            for (int i = 0; i < amount; i++)
+            return op;
+        }
+
+        private static int enterNumber(string message)
+        {
+            int number;
+            do
             {
-                Console.WriteLine("Please enter " + (i+1) + " number");
-                numbers[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine(message);
+            } while (!int.TryParse(Console.ReadLine(), out number));
+            return number;
+        }
+
+
+        private static int[] enterNumberArray(string op)
+        {
+            int amount = enterNumber("How many numbers do you want to "+ op +"?");
+            int[] numbers = new int[amount];
+            int j;
+            for (int i = 0;
+                i < amount;
+                i++)
+            {
+                numbers[i] = enterNumber("Please enter number " + (i+1) + ": ");
+
             }
 
+            return numbers;
+        }
+
+        private static double performCalculation(string op, int[] numbers)
+        {
+            
             double result = numbers[0];
-            for (int i = 1; i < numbers.Length; i++)
+            for (int i = 1;
+                i < numbers.Length;
+                i++)
             {
                 if (op == "+")
                 {
                     result += numbers[i];
-                } else if(op == "-")
+                }
+                else if (op == "-")
                 {
                     result -= numbers[i];
-                }else if (op == "*")
+                }
+                else if (op == "*")
                 {
                     result *= numbers[i];
-                }else if (op == "/")
-                {
-                    result/=numbers[i];
                 }
+                else if (op == "/")
+                {
+                    result /= numbers[i];
+                }
+
+                
             }
-            Console.WriteLine(numbers[0]);
-            Console.WriteLine(numbers[1]);
-            Console.WriteLine(result);
+            return result;
         }
-    }
-}
+
+        private static void calculateResult()
+        {
+            string op = enterOp();
+            int[] numbers = enterNumberArray(op);
+            double result = performCalculation(op, numbers);
+            Console.Write("The answer is " + result);
+            Console.WriteLine();
+        }
+    }   
+        }
+
+
+        
+
+      
+
+        
+
 
 
 
