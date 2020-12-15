e3694ccd01c818a1ace3ae42125caeca50188a60
tatyana rutkowska <kotita49@hotmail.com>
2020-12-15 11:41:35 +0000

    Added date calculator

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,8 +10,27 @@ namespace Calculator
             PrintWelcomeMessage();
             while (true)
             {
-                calculateResult();
+                int calculationMode = AskForCalculationMode();
+                if (calculationMode == NumberCalculator)
+                {
+                    calculateNumberResult();
+                }
+                else
+                {
+                    calculateDate();
+                }
+
             }
+        }
+
+
+        private const int NumberCalculator = 1;
+        private const int DateCalculator = 2;
+
+        private static int AskForCalculationMode()
+        {
+            int mode = enterNumber("What calculator do you want? 1 - Numbers, 2 - Dates");
+            return mode;
         }
 
         private static void PrintWelcomeMessage()
@@ -33,20 +52,21 @@ namespace Calculator
             {
                 Console.WriteLine(message);
             } while (!int.TryParse(Console.ReadLine(), out number));
+
             return number;
         }
 
 
         private static int[] enterNumberArray(string op)
         {
-            int amount = enterNumber("How many numbers do you want to "+ op +"?");
+            int amount = enterNumber("How many numbers do you want to " + op + "?");
             int[] numbers = new int[amount];
             int j;
             for (int i = 0;
                 i < amount;
                 i++)
             {
-                numbers[i] = enterNumber("Please enter number " + (i+1) + ": ");
+                numbers[i] = enterNumber("Please enter number " + (i + 1) + ": ");
 
             }
 
@@ -55,7 +75,7 @@ namespace Calculator
 
         private static double performCalculation(string op, int[] numbers)
         {
-            
+
             double result = numbers[0];
             for (int i = 1;
                 i < numbers.Length;
@@ -78,12 +98,13 @@ namespace Calculator
                     result /= numbers[i];
                 }
 
-                
+
             }
+
             return result;
         }
 
-        private static void calculateResult()
+        private static void calculateNumberResult()
         {
             string op = enterOp();
             int[] numbers = enterNumberArray(op);
@@ -91,9 +112,29 @@ namespace Calculator
             Console.Write("The answer is " + result);
             Console.WriteLine();
         }
-    }   
+
+        private static DateTime enterDate()
+        {
+            DateTime mydate;
+            do
+            {
+                Console.WriteLine("Please enter a date");
+            } while (!DateTime.TryParse(Console.ReadLine(), out mydate));
+
+            return mydate;
+
         }
 
+        private static void calculateDate()
+        {
+            DateTime mydate = enterDate();
+            int daysToAdd = enterNumber("Please enter a number of days to add:");
+DateTime resultDate = mydate.AddDays(daysToAdd);
+Console.Write("The answer is "+ resultDate);
+Console.WriteLine();
+        }
+    }
+}
 
         
 
