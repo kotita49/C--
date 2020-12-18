b5113faeb769eb8d41db4925fef1418c11c17919
tatyana rutkowska <kotita49@hotmail.com>
2020-12-18 12:11:07 +0000

    Separated into classes

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -3,79 +3,55 @@ using System.Data.Common;
 
 namespace Calculator
 {
-    class Program
+    class Start
     {
-        static void Main(string[] args)
-        {
-            PrintWelcomeMessage();
-            while (true)
-            {
-                int calculationMode = AskForCalculationMode();
-                if (calculationMode == NumberCalculator)
-                {
-                    calculateNumberResult();
-                }
-                else
-                {
-                    calculateDate();
-                }
-
-            }
-        }
-
-
-        private const int NumberCalculator = 1;
-        private const int DateCalculator = 2;
-
-        private static int AskForCalculationMode()
-        {
-            int mode = enterNumber("What calculator do you want? 1 - Numbers, 2 - Dates");
-            return mode;
-        }
-
-        private static void PrintWelcomeMessage()
+        public static void PrintWelcomeMessage()
         {
             Console.WriteLine("Welcome to the calculator!");
         }
 
-        private static string enterOp()
+        public int AskForCalculationMode()
+        {
+            Console.WriteLine("What calculator do you want? " +
+                              "1 - Numbers, " +
+                              "2 - Dates");
+            int mode;
+            while (!int.TryParse(Console.ReadLine(), out mode)) ;
+            return mode;
+        }
+
+       }
+
+    class NumberCalculator
+    {
+        public string enterOp()
         {
             Console.Write("Please enter an operator");
             string op = Console.ReadLine();
             return op;
         }
 
-        private static int enterNumber(string message)
+        public int[] enterNumberArray(string op)
         {
-            int number;
-            do
-            {
-                Console.WriteLine(message);
-            } while (!int.TryParse(Console.ReadLine(), out number));
-
-            return number;
-        }
-
-
-        private static int[] enterNumberArray(string op)
-        {
-            int amount = enterNumber("How many numbers do you want to " + op + "?");
+            Console.WriteLine("How many numbers do you want to " + op + "?");
+            int amount = int.Parse(Console.ReadLine());
             int[] numbers = new int[amount];
-            int j;
             for (int i = 0;
                 i < amount;
                 i++)
             {
-                numbers[i] = enterNumber("Please enter number " + (i + 1) + ": ");
-
+                int number;
+               do
+               { Console.WriteLine("Please enter " + (i + 1) + " number");
+                   
+               } while (!int.TryParse(Console.ReadLine(), out number));
+               numbers[i] = number;
             }
 
             return numbers;
         }
-
-        private static double performCalculation(string op, int[] numbers)
+        public double performCalculation(string op, int[] numbers)
         {
-
             double result = numbers[0];
             for (int i = 1;
                 i < numbers.Length;
@@ -104,37 +80,68 @@ namespace Calculator
             return result;
         }
 
-        private static void calculateNumberResult()
-        {
-            string op = enterOp();
-            int[] numbers = enterNumberArray(op);
-            double result = performCalculation(op, numbers);
-            Console.Write("The answer is " + result);
-            Console.WriteLine();
-        }
+    }
 
-        private static DateTime enterDate()
+
+    class Program
+    {
+
+        static void Main(string[] args)
         {
-            DateTime mydate;
-            do
+            Start myStart = new Start();
+            Start.PrintWelcomeMessage();
+            while (true)
             {
-                Console.WriteLine("Please enter a date");
-            } while (!DateTime.TryParse(Console.ReadLine(), out mydate));
 
-            return mydate;
-
+                if (myStart.AskForCalculationMode() == NumberCalculator)
+                {
+                    NumberCalculator myNum = new NumberCalculator();
+                    string op = myNum.enterOp();
+                    int[] numbers = myNum.enterNumberArray(op);
+                    Console.WriteLine("The answer is {0} .", myNum.performCalculation(op, numbers));
+                }
+                else
+                {
+                    DateCalculator newDate = new DateCalculator();
+                    DateTime myDate = newDate.EnterDate();
+                    DateTime resultDate = newDate.CalculateDate(myDate);
+                    Console.Write("The answer is "+ resultDate);
+                }
+                Console.WriteLine();
+            }
         }
 
-        private static void calculateDate()
-        {
-            DateTime mydate = enterDate();
-            int daysToAdd = enterNumber("Please enter a number of days to add:");
-DateTime resultDate = mydate.AddDays(daysToAdd);
-Console.Write("The answer is "+ resultDate);
-Console.WriteLine();
-        }
+        private const int NumberCalculator = 1;
+        private const int DateCalculator = 2;
+
     }
 }
+
+class DateCalculator
+{
+    public DateTime EnterDate()
+    {
+        DateTime mydate;
+        do
+        {
+            Console.WriteLine("Please enter a date");
+        } while (!DateTime.TryParse(Console.ReadLine(), out mydate));
+
+        return mydate;
+    }
+
+    public DateTime CalculateDate(DateTime mydate)
+    {
+       Console.WriteLine("Please enter a number of days to add:");
+        int daysToAdd = int.Parse(Console.ReadLine());
+        DateTime resultDate = mydate.AddDays(daysToAdd);
+        return resultDate;
+       }
+        
+    }
+
+    
+
 
         
 
